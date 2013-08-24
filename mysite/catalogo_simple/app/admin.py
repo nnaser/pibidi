4e218d0c1c17dcf4f381d0a44cc3c9f5bd6107cb
nnaser <nicolas.naser@usach.cl>
2013-08-24 19:55:29 -0400

    Revert "commit 5"
    This reverts commit 3082a54c5b46c3c37acb039de695d29c63c64689.

--- mysite/catalogo_simple/app/admin.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from django.contrib import admin
-from app.models import Producto, Comentario
-
-admin.site.register(Producto)
-admin.site.register(Comentario)
--- mysite/catalogo_simple/catalogo/static/js/script.js
+++
@@ -1,4 +0,0 @@
-/*
- * @author: Danilo Aburto Vivians   danilo dot aburto at requies dot cl
- * @author: Camilo Farfán Pérez     camilo dot farfan at usach dot cl
- */
